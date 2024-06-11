3b991f601386453b3cc1e85a2de111dcfcbd9b1d
Niels Demeyer <niels.demeyer99@gmail.com>
2024-06-11 20:23:04 +0200

    changing the navigation

--- app.py
+++ app.py
@@ -12,31 +12,20 @@ def main():
     db.refresh_images()
     ui = UI(db)
 
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        if st.button("Play!"):
-            st.session_state.page = "play"
-        st.write("Start a new game")
-    with col2:
-        if st.button("Contribute"):
-            st.session_state.page = "contribute"
-        st.write("Add your photos to the game")
-    with col3:
-        if st.button("Leaderboard"):
-            st.session_state.page = "leaderboard"
-        st.write("Take a peek at your competition")
+    pages = {
+        "Home": ui.display_home_page,
+        "Play": ui.display_play_page,
+        "Contribute": ui.display_contribute_page,
+        "Leaderboard": ui.display_leaderboard_page,
+    }
 
-    if "page" not in st.session_state:
-        st.session_state.page = "home"
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(pages.keys()))
 
-    if st.session_state.page == "play":
-        ui.display_play_page()
-    elif st.session_state.page == "contribute":
-        ui.display_contribute_page()
-    elif st.session_state.page == "leaderboard":
-        ui.display_leaderboard_page()
-    else:
-        ui.display_home_page()
+    page = pages[selection]
+
+    if page:
+        page()
 
 
 if __name__ == "__main__":
