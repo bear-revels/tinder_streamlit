27ba9a21124c106e772383103d8b96e65d8bb982
Caro <158029184+CarolineVHK@users.noreply.github.com>
2024-06-12 16:48:15 +0200

    update issues game

--- pages/3_🏆_Leaderboard.py
+++ pages/3_🏆_Leaderboard.py
@@ -1,6 +1,9 @@
 import streamlit as st
 from utils.Ui_file import UI
+from utils.Database_file import Database
 
-board = UI()
+db = Database()
+db.refresh_images()
+board = UI(db)
 
 board.display_leaderboard_page()
