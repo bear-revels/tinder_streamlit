055a3f70f733f4f9762d9ec23f7a03617b3aa33f
Your Name <you@example.com>
2024-06-13 11:54:10 +0200

    archiving old files

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,7 +1,6 @@
 from .Game_file import Game
-from .Ui_file import UI
 from .Database_file import Database
 from .Contribute_file import Contribute
 from .Leaderboard_file import Leaderboard
 
-__all__ = ["Game", "Database", "UI", "Contribute", "Leaderboard"]
+__all__ = ["Game", "Database", "Contribute", "Leaderboard"]
