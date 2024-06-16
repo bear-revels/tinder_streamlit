1c57e92b2592556f81ed06583b4231b4363a62d2
Your Name <you@example.com>
2024-06-16 17:13:22 +0200

    update leaderboard

--- pages/3_🏆_Leaderboard.py
+++ pages/3_🏆_Leaderboard.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from utils.Leaderboard_file import Leaderboard
+from utils.Database_file import Database
 
+db = Database()
+db.refresh_active_status()
+db.refresh_images()
 board = Leaderboard()
 board.display_leaderboard_page()
