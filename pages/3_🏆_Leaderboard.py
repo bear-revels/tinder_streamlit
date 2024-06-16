87686cdc4a65e4f6701ea1910f633401dc008b82
Your Name <you@example.com>
2024-06-16 16:33:48 +0200

    cleanup code leaderboard page

--- pages/3_🏆_Leaderboard.py
+++ pages/3_🏆_Leaderboard.py
@@ -1,19 +1,5 @@
-# import streamlit as st
-# # from utils.Ui_file import UI
-# from utils.Leaderboard_file import Leaderboard
-# from utils.Database_file import Database
-
-# db = Database()
-# board = Leaderboard(db)
-
-# board.display_leaderboard_page()
-
 import streamlit as st
 from utils.Leaderboard_file import Leaderboard
 
-def main():
-    st.set_page_config(page_title="Leaderboard", layout="wide")
-    Leaderboard.display_leaderboard_page()
-
-if __name__ == "__main__":
-    main()
+board = Leaderboard()
+board.display_leaderboard_page()
